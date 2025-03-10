9f718b178b884074ebc64f2f03c0105f98b0b3b8
Reynold254 <reynold.burudi@student.moringaschool.com>
2025-03-10 23:58:36 +0200

    Known

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Function to create thank you cards
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// Function to count down to zero
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Example usage
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+countDown(10);
